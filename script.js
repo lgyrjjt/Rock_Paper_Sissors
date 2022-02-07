4176dbb617f6755939160f4f6a64a6e0f7bfbcec
John Trehearn <johntrehearn@gmail.com>
2022-02-07 16:19:41 +0200

    Added computerPlay function

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("Hello World Piglet");
+
+
+
+
+let item = ["Rock", "Paper", "Sissors"];
+
+    function computerPlay() {
+        
+        let computerChoice = item[Math.floor(Math.random() * item.length)];
+        console.log(item);
+        console.log(computerChoice);
+    }
+
+
+
+
+
+
+
