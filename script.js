a9c087086cd07864e5eabedf505f0c1262874f0f
John Trehearn <johntrehearn@gmail.com>
2022-02-07 16:29:40 +0200

    Added return to function

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ let item = ["Rock", "Paper", "Sissors"];
     function computerPlay() {
         
         let computerChoice = item[Math.floor(Math.random() * item.length)];
+        
         console.log(item);
         console.log(computerChoice);
+        return(item);
     }
 
 
