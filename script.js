39157120ddf42b18184877416d98407abe167a2b
John Trehearn <johntrehearn@gmail.com>
2022-02-07 17:50:45 +0200

    Single choice working git add .

--- script.js
+++ script.js
@@ -2,17 +2,30 @@ console.log("Hello World Piglet");
 
 
 
-
 let item = ["Rock", "Paper", "Sissors"];
+let lose = ["Bad luck you lose!"]
 
     function computerPlay() {
         
         let computerChoice = item[Math.floor(Math.random() * item.length)];
         
-        console.log(item);
+       
         console.log(computerChoice);
         return(item);
     }
+
+computerPlay();
+
+let playerSelection = window.prompt("Please enter your selection of either Rock, Paper or Sissors: ");
+console.log(playerSelection)
+
+function game() {
+    if (playerSelection === "Rock") {
+        console.log("Bad Luck you lose!!!!!")
+    }
+}
+
+game();
 
 
 
