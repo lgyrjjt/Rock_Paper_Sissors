6ad5ba862864d33028507325b9539c49ee7ac496
John Trehearn <johntrehearn@gmail.com>
2022-02-07 22:30:40 +0200

    One round game tested

---
+++ Old/script.js
@@ -0,0 +1,13 @@
+let item = ["Rock", "Paper", "Sissors"];
+
+const computerChoice = item[Math.floor(Math.random() * item.length)];
+
+console.log(computerChoice);
+
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,34 +1,52 @@
-console.log("Hello World Piglet");
-
+let comS = ("Computer Says: ");
 
 
 let item = ["Rock", "Paper", "Sissors"];
-let lose = ["Bad luck you lose!"]
 
-    function computerPlay() {
-        
-        let computerChoice = item[Math.floor(Math.random() * item.length)];
-        
-       
-        console.log(computerChoice);
-        return(item);
-    }
+const computerPlay = item[Math.floor(Math.random() * item.length)];
+console.log(comS + computerPlay);
 
-computerPlay();
 
-let playerSelection = window.prompt("Please enter your selection of either Rock, Paper or Sissors: ");
+let playS = ("Player Says: ");
+
+let playerSelection1 = window.prompt("Please enter your selection of either Rock, Paper or Sissors: ");
+
+const ps = playerSelection1.charAt(0).toUpperCase();
+const psrest = playerSelection1.slice(1).toLowerCase();
+playerSelection = ps + psrest
+
+console.log(playS + playerSelection1)
 console.log(playerSelection)
 
-function game() {
-    if (playerSelection === "Rock") {
-        console.log("Bad Luck you lose!!!!!")
-    }
+if (playerSelection === "Rock" && computerPlay === "Paper") {
+    console.log("You Lose ööööööööö!!!!!!!!!!!!!");
+}   else if (playerSelection === "Rock" && computerPlay === "Rock") {
+        console.log("It is a Draw");
+}   else if (playerSelection === "Rock" && computerPlay === "Sissors") {
+        console.log("You Win!!!!!!! ööööööööö");
+}    else if (playerSelection === "Paper" && computerPlay === "Rock") {
+        console.log("Ya win this one!!!!!!");
+}   else if (playerSelection === "Paper" && computerPlay === "Paper") {
+        console.log("Its a Draw!Ö!Ö!Ö!Ö!Ö!Ö!Ö!");
+}   else if (playerSelection === "Paper" && computerPlay === "Sissors") {
+        console.log ("You Lose ö!ö!ö!Ö!ö!Ö1Ö!ö!Ö1Ö!Ö!Ö1ö");
+}   else if (playerSelection === "Sissors" && computerPlay === "Paper") {
+        console.log("You win this one!!!!!ääääääärrrrrr");
+}   else if (playerSelection === "Sissors" && computerPlay === "Rock") {
+        console.log("Looooooooooooser ö!Ö!Ö!Ö!Ö!Ö!Ö!Ö!Ö!Ö");
+}   else if (playerSelection === "Sissors" && computerPlay === "Sissors") {
+        console.log("It is a draw!");
+
+    
+
+    
+    
+
+
+   
+        
+    
 }
-
-game();
-
-
-
 
 
 
