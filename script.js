9bbb7a35b323b2985a37558bf7755faf7c9bbe99
John Trehearn <johntrehearn@gmail.com>
2022-02-08 18:25:37 +0200

    Both functions now working

--- script.js
+++ script.js
@@ -5,44 +5,50 @@ function computerPlay() {
         return item[Math.floor(Math.random() * item.length)];
 }
 
-let computerChoice = computerPlay();
+var computerChoice = computerPlay();
 
 console.log(comS + computerChoice);
 
-
 let playS = ("Player Says: ");
 
-let playerSelection1 = window.prompt("Please enter your selection of either Rock, Paper or Sissors: ");
+        let playerSelection1 = window.prompt("Please enter your selection of either Rock, Paper or Sissors: ");
 
-const ps = playerSelection1.charAt(0).toUpperCase();
-const psrest = playerSelection1.slice(1).toLowerCase();
-playerSelection = ps + psrest
+        const ps = playerSelection1.charAt(0).toUpperCase();
+        const psrest = playerSelection1.slice(1).toLowerCase();
+        playerSelection = ps + psrest
 
-console.log(playS + playerSelection1)
-console.log(playerSelection)
+        console.log(playS + playerSelection1)
+        console.log(playerSelection)
+        
 
-if (playerSelection === "Rock" && computerChoice === "Paper") {
-    console.log("You Lose ööööööööö!!!!!!!!!!!!!");
-}   else if (playerSelection === "Rock" && computerChoice === "Rock") {
-        console.log("It is a Draw");
-}   else if (playerSelection === "Rock" && computerChoice === "Sissors") {
-        console.log("You Win!!!!!!! ööööööööö");
-}    else if (playerSelection === "Paper" && computerChoice === "Rock") {
-        console.log("Ya win this one!!!!!!");
-}   else if (playerSelection === "Paper" && computerChoice === "Paper") {
-        console.log("Its a Draw!Ö!Ö!Ö!Ö!Ö!Ö!Ö!");
-}   else if (playerSelection === "Paper" && computerChoice === "Sissors") {
-        console.log ("You Lose ö!ö!ö!Ö!ö!Ö1Ö!ö!Ö1Ö!Ö!Ö1ö");
-}   else if (playerSelection === "Sissors" && computerChoice === "Paper") {
-        console.log("You win this one!!!!!ääääääärrrrrr");
-}   else if (playerSelection === "Sissors" && computerChoice === "Rock") {
-        console.log("Looooooooooooser ö!Ö!Ö!Ö!Ö!Ö!Ö!Ö!Ö!Ö");
-}   else if (playerSelection === "Sissors" && computerChoice === "Sissors") {
-        console.log("It is a draw!");
+let winner = play();
 
-    
+function play() {
 
-    
+
+
+        if (playerSelection === "Rock" && computerChoice === "Paper") {
+        console.log("You Lose ööööööööö!!!!!!!!!!!!!");
+        }   else if (playerSelection === "Rock" && computerChoice === "Rock") {
+                console.log("It is a Draw");
+        }   else if (playerSelection === "Rock" && computerChoice === "Sissors") {
+                console.log("You Win!!!!!!! ööööööööö");
+        }    else if (playerSelection === "Paper" && computerChoice === "Rock") {
+                console.log("Ya win this one!!!!!!");
+        }   else if (playerSelection === "Paper" && computerChoice === "Paper") {
+                console.log("Its a Draw!Ö!Ö!Ö!Ö!Ö!Ö!Ö!");
+        }   else if (playerSelection === "Paper" && computerChoice === "Sissors") {
+                console.log ("You Lose ö!ö!ö!Ö!ö!Ö1Ö!ö!Ö1Ö!Ö!Ö1ö");
+        }   else if (playerSelection === "Sissors" && computerChoice === "Paper") {
+                console.log("You win this one!!!!!ääääääärrrrrr");
+        }   else if (playerSelection === "Sissors" && computerChoice === "Rock") {
+                console.log("Looooooooooooser ö!Ö!Ö!Ö!Ö!Ö!Ö!Ö!Ö!Ö");
+        }   else if (playerSelection === "Sissors" && computerChoice === "Sissors") {
+                console.log("It is a draw!");
+
+}
+
+
     
 
 
