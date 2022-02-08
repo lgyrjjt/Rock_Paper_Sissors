aea5b74fb0db22b3f49a2eef7eaff31ef215fbe0
John Trehearn <johntrehearn@gmail.com>
2022-02-08 18:36:58 +0200

    Comments Added

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
+/* Info for console */
+
 let comS = ("Computer Says: ");
+
+/* Computer's choice for each game */
 
 function computerPlay() {
         let item = ["Rock", "Paper", "Sissors"];
         return item[Math.floor(Math.random() * item.length)];
 }
 
+/* Makes function to global variable */
+
 var computerChoice = computerPlay();
+
+/* Log for testing */
 
 console.log(comS + computerChoice);
 
+/* Info for console */
+
 let playS = ("Player Says: ");
 
-        let playerSelection1 = window.prompt("Please enter your selection of either Rock, Paper or Sissors: ");
+/* Player input */
 
-        const ps = playerSelection1.charAt(0).toUpperCase();
-        const psrest = playerSelection1.slice(1).toLowerCase();
-        playerSelection = ps + psrest
+let playerSelection1 = window.prompt("Please enter your selection of either Rock, Paper or Sissors: ");
 
-        console.log(playS + playerSelection1)
-        console.log(playerSelection)
+const ps = playerSelection1.charAt(0).toUpperCase();
+const psrest = playerSelection1.slice(1).toLowerCase();
+playerSelection = ps + psrest
+
+/* Logs the answers for testing */
+
+console.log(playS + playerSelection1)
+console.log(playerSelection)
         
-
+/* Calls Function */
 let winner = play();
+
+/* Game play function */
 
 function play() {
 
@@ -47,13 +63,6 @@ function play() {
                 console.log("It is a draw!");
 
 }
-
-
-    
-
-
-   
-        
     
 }
 
