38b8a2fab9105190c5cbd0c4ff650fdce287b973
John Trehearn <johntrehearn@gmail.com>
2022-02-08 17:38:33 +0200

    Function now working computerChoice

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let comS = ("Computer Says: ");
 
+function computerPlay() {
+        let item = ["Rock", "Paper", "Sissors"];
+        return item[Math.floor(Math.random() * item.length)];
+}
 
-let item = ["Rock", "Paper", "Sissors"];
+let computerChoice = computerPlay();
 
-const computerPlay = item[Math.floor(Math.random() * item.length)];
-console.log(comS + computerPlay);
+console.log(comS + computerChoice);
 
 
 let playS = ("Player Says: ");
@@ -18,23 +21,23 @@ playerSelection = ps + psrest
 console.log(playS + playerSelection1)
 console.log(playerSelection)
 
-if (playerSelection === "Rock" && computerPlay === "Paper") {
+if (playerSelection === "Rock" && computerChoice === "Paper") {
     console.log("You Lose ööööööööö!!!!!!!!!!!!!");
-}   else if (playerSelection === "Rock" && computerPlay === "Rock") {
+}   else if (playerSelection === "Rock" && computerChoice === "Rock") {
         console.log("It is a Draw");
-}   else if (playerSelection === "Rock" && computerPlay === "Sissors") {
+}   else if (playerSelection === "Rock" && computerChoice === "Sissors") {
         console.log("You Win!!!!!!! ööööööööö");
-}    else if (playerSelection === "Paper" && computerPlay === "Rock") {
+}    else if (playerSelection === "Paper" && computerChoice === "Rock") {
         console.log("Ya win this one!!!!!!");
-}   else if (playerSelection === "Paper" && computerPlay === "Paper") {
+}   else if (playerSelection === "Paper" && computerChoice === "Paper") {
         console.log("Its a Draw!Ö!Ö!Ö!Ö!Ö!Ö!Ö!");
-}   else if (playerSelection === "Paper" && computerPlay === "Sissors") {
+}   else if (playerSelection === "Paper" && computerChoice === "Sissors") {
         console.log ("You Lose ö!ö!ö!Ö!ö!Ö1Ö!ö!Ö1Ö!Ö!Ö1ö");
-}   else if (playerSelection === "Sissors" && computerPlay === "Paper") {
+}   else if (playerSelection === "Sissors" && computerChoice === "Paper") {
         console.log("You win this one!!!!!ääääääärrrrrr");
-}   else if (playerSelection === "Sissors" && computerPlay === "Rock") {
+}   else if (playerSelection === "Sissors" && computerChoice === "Rock") {
         console.log("Looooooooooooser ö!Ö!Ö!Ö!Ö!Ö!Ö!Ö!Ö!Ö");
-}   else if (playerSelection === "Sissors" && computerPlay === "Sissors") {
+}   else if (playerSelection === "Sissors" && computerChoice === "Sissors") {
         console.log("It is a draw!");
 
     
